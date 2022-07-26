b4e93ef3b56fd3b37e7b1dfb52f6c64b76a7b2bf
Patrick448 <patrickcarvalho448@gmail.com>
2022-07-26 16:36:45 -0300

    project creation

---
+++ src/main/java/com/example/agricultureoptimizerbackend/AgricultureOptimizerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.agricultureoptimizerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgricultureOptimizerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgricultureOptimizerBackendApplication.class, args);
+    }
+
+}
