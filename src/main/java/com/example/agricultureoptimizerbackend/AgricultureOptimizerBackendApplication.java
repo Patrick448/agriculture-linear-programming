1ba6fde698f248708a5904ea52c3a178148c9c54
Patrick448 <patrickcarvalho448@gmail.com>
2022-07-29 09:56:06 -0300

    instalação do glpk

--- src/main/java/com/example/agricultureoptimizerbackend/AgricultureOptimizerBackendApplication.java
+++ src/main/java/com/example/agricultureoptimizerbackend/AgricultureOptimizerBackendApplication.java
@@ -1,10 +1,21 @@
 package com.example.agricultureoptimizerbackend;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.gnu.glpk.GLPK;
+import org.gnu.glpk.GLPKConstants;
+import org.gnu.glpk.GlpkException;
+import org.gnu.glpk.SWIGTYPE_p_double;
+import org.gnu.glpk.SWIGTYPE_p_int;
+import org.gnu.glpk.glp_prob;
+import org.gnu.glpk.glp_smcp;
 
 @SpringBootApplication
 public class AgricultureOptimizerBackendApplication {
+
+    public glp_prob lp = GLPK.glp_create_prob();
 
     public static void main(String[] args) {
         SpringApplication.run(AgricultureOptimizerBackendApplication.class, args);
