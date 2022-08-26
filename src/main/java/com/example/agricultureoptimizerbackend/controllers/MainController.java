2cd2407cf0e9b078f05023b633720694fa573cbc
Patrick448 <patrickcarvalho448@gmail.com>
2022-08-26 13:18:52 -0300

    ajustes no retorno e persistencia de solução

--- src/main/java/com/example/agricultureoptimizerbackend/controllers/MainController.java
+++ src/main/java/com/example/agricultureoptimizerbackend/controllers/MainController.java
@@ -5,10 +5,7 @@ import com.example.agricultureoptimizerbackend.dto.InputDataDTO;
 import com.example.agricultureoptimizerbackend.dto.SolutionCropDTO;
 import com.example.agricultureoptimizerbackend.dto.SolutionDTO;
 import com.example.agricultureoptimizerbackend.model.*;
-import com.example.agricultureoptimizerbackend.services.CropService;
-import com.example.agricultureoptimizerbackend.services.InputDataService;
-import com.example.agricultureoptimizerbackend.services.SolutionCropService;
-import com.example.agricultureoptimizerbackend.services.SolutionService;
+import com.example.agricultureoptimizerbackend.services.*;
 import com.example.agricultureoptimizerbackend.utils.DataFileReader;
 import org.gnu.glpk.*;
 import org.hibernate.Criteria;
@@ -34,6 +31,8 @@ public class MainController {
     SolutionCropService solutionCropService;
     @Autowired
     InputDataService inputDataService;
+    @Autowired
+    FieldService fieldService;
 
     @GetMapping(value = "/test")
     public ResponseEntity<String> test(HttpServletResponse response) {
@@ -127,7 +126,7 @@ public class MainController {
         for (int i = 0; i < prices.length; i++) {
             solution[i] = GLPK.glp_get_col_prim(lp, i + 1);
             Crop crop = new Crop("couve", prices[i], costs[i], 1.0, 1.0);
-            solutionCrops.add(new SolutionCrop((int) solution[i], solutionObject, crop, crop.getPrice(), crop.getSpace(), crop.getTime(), crop.getCost()));
+           solutionCrops.add(new SolutionCrop(null, (int) solution[i], crop.getPrice(),crop.getSpace(), crop.getTime(), crop.getCost(), 0, solutionObject, crop, new Field(null, 0, "X") ));
         }
 
         //solutionService.save(solutionObject);
@@ -208,13 +207,13 @@ public class MainController {
         for (int i = 0; i < cropList.size(); i++) {
             double amount = GLPK.glp_mip_col_val(lp, i + 1);
             Crop crop = cropList.get(i);
-            SolutionCrop solutionCrop = new SolutionCrop((int) amount, solutionObject, crop, crop.getPrice(), crop.getSpace(), crop.getTime(), crop.getCost());
+            SolutionCrop solutionCrop = new SolutionCrop(null, (int) amount,crop.getPrice(), crop.getSpace(), crop.getTime(), crop.getCost(), 0,  solutionObject, crop, new Field(null, 0, "X"));
             solutionCrops.add(solutionCrop);
 
             solution[i] = amount;
         }
 
-        solutionService.save(solutionObject);
+        //solutionService.save(solutionObject);
         System.out.println("z = " + z + "\n");
         DataFileReader.printArray(solution);
         GLPK.glp_delete_prob(lp);
@@ -225,56 +224,54 @@ public class MainController {
     @PostMapping(value = "/solve-new")
     public ResponseEntity<SolutionDTO> solveUsingDB2(HttpServletResponse response, @RequestBody InputData inputData) {
 
+        List<Crop> cropList = inputData.getSelectedCrops();
+        List<Field> fields = inputData.getFields();
+        //fieldService.save(fields);
+
         glp_prob lp;
         SWIGTYPE_p_int ia;
         SWIGTYPE_p_int ja;
         SWIGTYPE_p_double d;
 
         lp = GLPK.glp_create_prob();
-        d = GLPK.new_doubleArray(1000);
-        ia = GLPK.new_intArray(1000);
-        ja = GLPK.new_intArray(1000);
+
         GLPK.glp_set_prob_name(lp, "sample");
         GLPK.glp_set_obj_dir(lp, GLPKConstants.GLP_MAX);
 
-        List<Crop> cropList = inputData.getSelectedCrops();
-        GLPK.glp_add_cols(lp, cropList.size()*3*3);
 
-
-
-        List<Field> fields = new ArrayList<Field>();
-        fields.add(new Field(null, 10, "a1"));
-        fields.add(new Field(null, 10, "a2"));
-        fields.add(new Field(null, 10, "a3"));
-
-
-        int index=1;
-        int fieldRestrictionCounter=1;
-        int rotationRestrictionCounter=1;
-        int matrixCounter=0;
+        int index = 1;
+        int fieldRestrictionCounter = 1;
+        int rotationRestrictionCounter = 1;
+        int matrixCounter = 0;
         int M = 12;
-
-        GLPK.glp_add_rows(lp, 1 + 3*3);
 
         int numI = cropList.size();
         int numJ = fields.size();
         int numK = 3;
-        int kSectionSize = numI*numJ;
+        int kSectionSize = numI * numJ;
+        int totalFieldRestr = numJ * numK;
+        int totalElements = numI*numJ*numK;
 
-        for(int k=0; k< numK; k++){
-            for(int j=0; j<numJ; j++){
+        GLPK.glp_add_rows(lp, 1 + totalFieldRestr);
+        GLPK.glp_add_cols(lp, numI*numJ*numK);
+        d = GLPK.new_doubleArray(1000000);
+        ia = GLPK.new_intArray(1000000);
+        ja = GLPK.new_intArray(1000000);
+
+        for (int k = 0; k < numK; k++) {
+            for (int j = 0; j < numJ; j++) {
 
                 double fieldSize = fields.get(j).getSize();
-                for(int i=0; i<numI; i++){
+                for (int i = 0; i < numI; i++) {
                     System.out.print(i + "\t");
 
                     double profit = cropList.get(i).getProfit();
                     double cost = cropList.get(i).getCost();
                     double space = cropList.get(i).getSpace();
                     double time = cropList.get(i).getTime();
-                    double fieldProfitByTimeFrame = (fieldSize/space)*profit/time;
-                    double fieldCostByTimeFrame = (fieldSize/space)*cost/time;
-                    int columnIndex = (i+1)+3*(j)+9*(k);
+                    double fieldProfitByTimeFrame = (fieldSize / space) * profit / time;
+                    double fieldCostByTimeFrame = (fieldSize / space) * cost / time;
+                    int columnIndex = (i + 1) + (numI * (j)) + (kSectionSize * (k));
 
 
                     //coeficientes e posições da restrição de investimento
@@ -284,112 +281,83 @@ public class MainController {
 
                     index++;
 
-
                     //coeficientes e posições das restrições de canteiro
                     GLPK.doubleArray_setitem(d, index, 1.0);
-                    GLPK.intArray_setitem(ia, index, 1+fieldRestrictionCounter);
+                    GLPK.intArray_setitem(ia, index, 1 + fieldRestrictionCounter);
                     GLPK.intArray_setitem(ja, index, columnIndex);
 
                     index++;
 
-        //***** restrições de rotação
-
+                    //***** restrições de rotação
 
                     //não plantar sem rotacionar
-                    if(k < 3-time) {
+                    //se indice k do elemento atual é menor que o tempo total menos o tempo da cultura
+                    if (k < numK - time) {
+
                         for (int x = 0; x < (int) time /* && x + (int) time <= 3*/; x++) {
                             GLPK.doubleArray_setitem(d, index, M);
-                            GLPK.intArray_setitem(ia, index, 9 + 1 + rotationRestrictionCounter);
-                            GLPK.intArray_setitem(ja, index, (i + 1) + 3 * (j) + 9 * (k+x));
+                            GLPK.intArray_setitem(ia, index, totalFieldRestr + 1 + rotationRestrictionCounter);
+                            GLPK.intArray_setitem(ja, index, (i + 1) + numI * (j) + kSectionSize * (k + x));
                             index++;
                         }
 
                         GLPK.doubleArray_setitem(d, index, 1);
-                        GLPK.intArray_setitem(ia, index, 9 + 1 + rotationRestrictionCounter);
-                        GLPK.intArray_setitem(ja, index, (i + 1) + 3 * (j) + 9 * (k+(int) time) );
+                        GLPK.intArray_setitem(ia, index, totalFieldRestr + 1 + rotationRestrictionCounter);
+                        GLPK.intArray_setitem(ja, index, (i + 1) + numI * (j) + kSectionSize * (k + (int) time));
                         index++;
 
                         GLPK.glp_add_rows(lp, 1);
-                        GLPK.glp_set_row_name(lp, 9 + 1 + rotationRestrictionCounter, "r" + rotationRestrictionCounter);
-                        GLPK.glp_set_row_bnds(lp, 9 + 1 + rotationRestrictionCounter, GLPKConstants.GLP_DB, 0, M * time);
+                        GLPK.glp_set_row_name(lp, totalFieldRestr + 1 + rotationRestrictionCounter, "r" + rotationRestrictionCounter);
+                        GLPK.glp_set_row_bnds(lp, totalFieldRestr + 1 + rotationRestrictionCounter, GLPKConstants.GLP_DB, 0, M * time);
 
                         rotationRestrictionCounter++;
 
                     }
 
-
-                    if(k==3-time-1){//não plantar sem que o tempo de cultivo caiba no período estipulado
+                    //não plantar sem que o tempo de cultivo caiba no período estipulado
+                    if (k == numK - time - 1) {
 
                         //o índice k vai de 3-time-1 até k+time
+                        // -Xi,j,(k+s-n-1) -...-Xi,j,(s-1) | n=time | s=numK
                         for (int x = 0; x < (int) time; x++) {
                             GLPK.doubleArray_setitem(d, index, -1);
-                            GLPK.intArray_setitem(ia, index, 9 + 1 + rotationRestrictionCounter);
-                            GLPK.intArray_setitem(ja, index, (i + 1) + 3 * (j) + 9 * (k+x));
+                            GLPK.intArray_setitem(ia, index, totalFieldRestr + 1 + rotationRestrictionCounter);
+                            GLPK.intArray_setitem(ja, index, (i + 1) + numI * (j) + kSectionSize * (k + x));
                             index++;
                         }
 
-                        GLPK.doubleArray_setitem(d, index, M-1);
-                        GLPK.intArray_setitem(ia, index, 9 + 1 + rotationRestrictionCounter);
-                        GLPK.intArray_setitem(ja, index, (i + 1) + 3 * (j) + 9 * (k+(int) time) );
+                        //ultimo termo da restrição: M(Xijk -1) | k=s=numK
+                        GLPK.doubleArray_setitem(d, index, M - 1);
+                        GLPK.intArray_setitem(ia, index, totalFieldRestr + 1 + rotationRestrictionCounter);
+                        GLPK.intArray_setitem(ja, index, (i + 1) + numI * (j) + kSectionSize * (k + (int) time));
                         index++;
 
+                        //cria espaço e dá nome à restrição
                         GLPK.glp_add_rows(lp, 1);
-                        GLPK.glp_set_row_name(lp, 9 + 1 + rotationRestrictionCounter, "r" + rotationRestrictionCounter);
-                        GLPK.glp_set_row_bnds(lp, 9 + 1 + rotationRestrictionCounter, GLPKConstants.GLP_DB, 0, M * time);
+                        GLPK.glp_set_row_name(lp, totalFieldRestr + 1 + rotationRestrictionCounter, "r" + rotationRestrictionCounter);
+                        GLPK.glp_set_row_bnds(lp, totalFieldRestr + 1 + rotationRestrictionCounter, GLPKConstants.GLP_DB, 0, M - time);
 
                         rotationRestrictionCounter++;
 
 
-
                     }
-        ///////***********
+                    ///////***********
 
 
-
-                    matrixCounter+=2;
+                    matrixCounter += 2;
 
                     //definindo colunas
-                    GLPK.glp_set_col_name(lp, (i+1)+3*(j)+9*(k), "x" + (i + 1) + "," + (j + 1) +"," + (k + 1));
-                    GLPK.glp_set_col_kind(lp, (i+1)+3*(j)+9*(k), GLPKConstants.GLP_BV);
-                    GLPK.glp_set_col_bnds(lp, (i+1)+3*(j)+9*(k), GLPKConstants.GLP_DB, 0, 1);
-                    GLPK.glp_set_obj_coef(lp, (i+1)+3*(j)+9*(k), fieldProfitByTimeFrame);
-
-
+                    GLPK.glp_set_col_name(lp, columnIndex, "x" + (i + 1) + "," + (j + 1) + "," + (k + 1));
+                    GLPK.glp_set_col_kind(lp, columnIndex, GLPKConstants.GLP_BV);
+                    GLPK.glp_set_col_bnds(lp, columnIndex, GLPKConstants.GLP_DB, 0, 1);
+                    GLPK.glp_set_obj_coef(lp, columnIndex, fieldProfitByTimeFrame);
 
                 }
 
-                GLPK.glp_set_row_name(lp, 1+fieldRestrictionCounter, "j"+fieldRestrictionCounter);
-                GLPK.glp_set_row_bnds(lp, 1+fieldRestrictionCounter, GLPKConstants.GLP_DB, 0, 1);
+                GLPK.glp_set_row_name(lp, 1 + fieldRestrictionCounter, "j" + fieldRestrictionCounter);
+                GLPK.glp_set_row_bnds(lp, 1 + fieldRestrictionCounter, GLPKConstants.GLP_DB, 0, 1);
 
                 fieldRestrictionCounter++;
-
-
-
-                //**************
-               /* for(int i=0; i<cropList.size(); i++) {
-                    double time = cropList.get(i).getTime();
-
-                    GLPK.doubleArray_setitem(d, index + 54, -(M * time));
-                    GLPK.intArray_setitem(ia, index + 54, 9 + 1 + rotationRestrictionCounter);
-                    GLPK.intArray_setitem(ja, index + 54, 0);
-
-                    index++;
-
-                    for (int x = 1; x < time; x++) {
-                        GLPK.doubleArray_setitem(d, index + 54, M);
-                        GLPK.intArray_setitem(ia, index + 54, 9 + 1 + rotationRestrictionCounter);
-                        GLPK.intArray_setitem(ja, index + 54, index);
-                        index++;
-                    }
-
-                    GLPK.glp_set_row_name(lp, 9 + 1 + rotationRestrictionCounter, "r" + rotationRestrictionCounter);
-                    GLPK.glp_set_row_bnds(lp, 9 + 1 + rotationRestrictionCounter, GLPKConstants.GLP_DB, 0, 0);
-
-                    rotationRestrictionCounter++;
-                }*/
-                //********************
-
-
 
                 System.out.println();
             }
@@ -397,17 +365,14 @@ public class MainController {
         }
 
 
+        //dá nome e limite à restrição de investimento
         double maxInvest = inputData.getBudget();
-       // double maxSpace = inputData.getSpace();
-
         GLPK.glp_set_row_name(lp, 1, "c");
         GLPK.glp_set_row_bnds(lp, 1, GLPKConstants.GLP_DB, 0.0, maxInvest);
 
-        //GLPK.glp_set_row_name(lp, 2, "s");
-        //GLPK.glp_set_row_bnds(lp, 2, GLPKConstants.GLP_UP, 0.0, maxSpace);
 
-        double[] solution = new double[27];
-        GLPK.glp_load_matrix(lp, index-1, ia, ja, d);
+        double[] solution = new double[numI*numJ*numK];
+        GLPK.glp_load_matrix(lp, index - 1, ia, ja, d);
         //printSystem(lp);
         GLPK.glp_write_lp(lp, null, "my_ourput.txt");
         GLPK.glp_simplex(lp, null);
@@ -426,25 +391,26 @@ public class MainController {
 
         for (int i = 0; i < solution.length; i++) {
             double value = GLPK.glp_mip_col_val(lp, i + 1);
-            String varName = GLPK.glp_get_col_name(lp, i+1);
+            String varName = GLPK.glp_get_col_name(lp, i + 1);
+            String commaSeparatedIndices = varName.substring(1);
+            String[] indices = commaSeparatedIndices.split(",");
+            int ii = Integer.parseInt(indices[0]);
+            int ij = Integer.parseInt(indices[1]);
+            int ik = Integer.parseInt(indices[2]);
 
-            if(value!=0.0){
-                String commaSeparatedIndices = varName.substring(1);
-                String[] indices = commaSeparatedIndices.split(",");
-                int ii = Integer.parseInt(indices[0]);
-                int ij = Integer.parseInt(indices[1]);
-                int ik = Integer.parseInt(indices[2]);
+            if (value != 0.0) {
                 System.out.println(varName);
-                System.out.println("Plantar " + cropList.get(ii-1).getName() + ", em " + fields.get(ij-1).getName() + " no tempo "+ (ik-1));
+                System.out.println("Plantar " + cropList.get(ii - 1).getName() + ", em " + fields.get(ij - 1).getName() + " no tempo " + (ik - 1));
+                Crop crop = cropList.get(ii - 1);
+                SolutionCrop solutionCrop = new SolutionCrop(null, (int) 0, crop.getPrice(), crop.getSpace(), crop.getTime(), crop.getCost(), ik-1,  solutionObject, crop, fields.get(ij-1));
+                solutionCrops.add(solutionCrop);
             }
-            //Crop crop = cropList.get(i);
-           // SolutionCrop solutionCrop = new SolutionCrop((int) amount, solutionObject, crop, crop.getPrice(), crop.getSpace(), crop.getTime(), crop.getCost());
-            //solutionCrops.add(solutionCrop);
+
 
             solution[i] = value;
         }
 
-        //solutionService.save(solutionObject);
+        solutionService.save(solutionObject);
         System.out.println("z = " + z + "\n");
         DataFileReader.printArray(solution);
         GLPK.glp_delete_prob(lp);
